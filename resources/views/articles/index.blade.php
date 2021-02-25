5739954a81adc5c0ab145683e8f83337d26a7f14
Andrea Iglio <andrea.iglio@gmail.com>
2021-02-25 19:41:45 +0100

    Implemented views/articles/partials/card.blade to have a reusable blade

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -10,24 +10,7 @@
 
 @foreach($articles as $article)
 
-<div class="card flex column">
-
-
-    <h3>Title: {{$article->title}}</h3>
-    <p>{{$article->content}}</p>
-    <h4>Author: {{$article->author}}</h4>
-
-    <h4 class="category">Category: {{$article->category->name}}</h4>
-
-    <div class="crud flex">
-        <button class="btn btn-dark"><a href="{{ route('articles.show', $article->id) }}"><i class="fas fa-eye"></i></a></button>
-        <button class="btn btn-warning" href=""><a href="{{ route('articles.edit', $article->id) }}"><i class="fas fa-edit"></i></a></button>
-
-    </div>
-
-
-
-</div>
+@include('articles.partials.card')
 
 @endforeach
 
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,24 +8,8 @@
 
 @section('content')
 
-<div class="card flex column">
+@include('articles.partials.card')
 
-
-    <h3>Title: {{$article->title}}</h3>
-    <p>{{$article->content}}</p>
-    <h4>Author: {{$article->author}}</h4>
-
-    <h4 class="category">Category: {{$article->category->name}}</h4>
-
-    <div class="crud flex">
-        <button class="btn btn-dark"><a href="{{ route('articles.show', $article->id) }}"><i class="fas fa-eye"></i></a></button>
-        <button class="btn btn-warning" href=""><a href="{{ route('articles.edit', $article->id) }}"><i class="fas fa-edit"></i></a></button>
-
-    </div>
-
-
-
-</div>
 
 
 @endsection
